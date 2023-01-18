d87ad408e84b2e363cbadcc98dbcf65ae62cb77a
opilane <opilane@HP-CZC0359NN7>
2023-01-18 15:07:28 +0200

    Add project files.

---
+++ DatabaseTask.Core/Domain/ConstructionCompany.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseTask.Core.Domain
+{
+    public class ConstructionCompany
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        public string CompanyName { get; set; }
+        public string Project { get; set; }
+
+        public int Employees { get; set; }
+        public int Specialists { get; set; }
+        public int Phone { get; set; }
+    }
+}
---
+++ DatabaseTask.Core/Domain/Project.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseTask.Core.Domain
+{
+    public class Project
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public string Task { get; set; }
+        public string ProjectName { get; set; }
+
+        public int EmployeeCount { get; set; }
+        public int ProjectCost { get; set; }
+
+        public DateTime Deadline { get; set; }
+
+        public ConstructionCompany ConstructionCompanyId { get; set; }
+    }
+}
